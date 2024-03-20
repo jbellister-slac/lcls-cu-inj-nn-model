41f5d54ed63297ab70491c8e0228824b79576355
jbellister-slac <jesseb@slac.stanford.edu>
2024-03-20 12:10:46 -0700

    Install versioneer.

--- lcls_cu_inj_nn_model/__init__.py
+++ lcls_cu_inj_nn_model/__init__.py
@@ -2,3 +2,6 @@ from lcls_cu_inj_nn_model.files import VARIABLE_FILE
 from lume_model.utils import variables_from_yaml
 
 INPUT_VARIABLES, OUTPUT_VARIABLES = variables_from_yaml(VARIABLE_FILE)
+
+from . import _version
+__version__ = _version.get_versions()['version']
